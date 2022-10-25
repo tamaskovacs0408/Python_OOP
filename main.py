d125637ea85f2ce3c529c5098eadf326acfc1327
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-25 15:00:45 +0200

    Update

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 class Item:
+  def __init__(self, name, price, quantity):
+      self.name = name
+      self.price = price
+      self.quantity = quantity
+    
   def calculate_total_price(self, x, y):
-    return x, y
+    return x * y
 
 
-item1 = Item()
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
-item1.calculate_total_price(item1.price, item1.quantiy)
+item1 = Item("Phone", 100, 5)
 
-item2 = Item()
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
+item2 = Item("Laptop", 1000, 3)
 
+item3 = Item("Camera", 1200, 4)
+
+print(f"The name of the product is {item1.name}.")
