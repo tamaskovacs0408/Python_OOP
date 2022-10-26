a574d191d4e2916725beb754a808b7303bae0c88
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-26 15:09:06 +0200

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Item:
   pay_rate = 0.8 # The pay rate after 20% discount
-  
+  all = []
   def __init__(self, name: str, price: float, quantity: int):
     #Received arguments validation
       assert price >= 0, f"Price {price} is not greater or equals to 0."
@@ -10,6 +10,9 @@ class Item:
       self.name = name
       self.price = price
       self.quantity = quantity
+      
+    # Actions to execute
+      Item.all.append(self)
     
   def calculate_total_price(self):
     return self.price * self.quantity
@@ -28,3 +31,8 @@ item5 = Item("Keyboard", 75, 5)
 # print(f"Instance level attributes: {item1.__dict__}")
 item1.apply_discount()
 print(item1.price)
+
+print(Item.all)
+
+for instance in Item.all:
+  print(instance.name)
